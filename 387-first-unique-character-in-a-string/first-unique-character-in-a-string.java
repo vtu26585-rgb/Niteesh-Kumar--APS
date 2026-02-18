d8445629fe706f9ad0e0629f0b1dc2911b2fb624
vtu26585-rgb <vtu26585@veltech.edu.in>
2026-02-18 21:14:53 +0530

    Time: 32 ms (31.11%) | Memory: 46.9 MB (71.35%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -1,14 +1,20 @@
-class Solution{
-public int firstUniqChar(String s){
-int[]freq=new int[26];
-for(char c:s.toCharArray()){
-freq[c-'a']++;
+
+
+class Solution {
+    public int firstUniqChar(String s) {
+        HashMap<Character, Integer> mp = new HashMap<>();
+
+        for (char a : s.toCharArray()) {
+            mp.put(a, mp.getOrDefault(a, 0) + 1);
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (mp.get(s.charAt(i)) == 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
 }
-for(int i=0;i<s.length();i++){
-if(freq[s.charAt(i)-'a']==1){
-return i;
-}
-}
-return -1;
-}
-}
+
