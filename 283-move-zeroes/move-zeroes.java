7480f2ee0252e9e980265889e0b4f14b669a65f6
vtu26585-rgb <vtu26585@veltech.edu.in>
2026-02-25 09:58:03 +0530

    Time: 2 ms (90.18%) | Memory: 47.6 MB (93.30%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,13 +1,12 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int j = 0; // Pointer to place the next non-zero element
+        int j = 0; 
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 0) {
-                // Swap current element with the element at index j 
-                int temp = nums[i];
+             int temp = nums[i];
                 nums[i] = nums[j];
                 nums[j] = temp;
-                j++;   // Move j to the next index for placing non-zero
+                j++;   
             }
         }
     }
