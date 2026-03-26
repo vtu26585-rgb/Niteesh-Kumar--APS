421f677648dc1720111633b3d7d0b095f8ad40f8
vtu26585-rgb <vtu26585@veltech.edu.in>
2026-03-26 09:23:13 +0530

    Time: 7 ms (99.91%) | Memory: 137.2 MB (99.80%) - LeetSync

---
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int pref[] = new int[nums.length];
+        int suf[] = new int[nums.length];
+
+        for(int i=0;i<nums.length;i++){
+            if(i%k==0){
+                pref[i]=nums[i];
+            }
+            else{
+                pref[i] = Math.max(pref[i-1],nums[i]);
+            }
+        }
+
+        suf[nums.length-1]=nums[nums.length-1];
+        for(int i=nums.length-2;i>=0;i--){
+            if((i+1)%k==0){
+                suf[i]=nums[i];
+            }
+            else{
+                suf[i] = Math.max(suf[i+1],nums[i]);
+            }
+        }
+
+        int ans[] = new int[nums.length-k+1];
+        for(int i=0;i<ans.length;i++){
+            ans[i] = Math.max(suf[i],pref[i+k-1]);
+        }
+        return ans;
+    }
+}
