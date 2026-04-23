a904c751b2288f456282dc572ab4db7750181b3d
vtu26585-rgb <vtu26585@veltech.edu.in>
2026-04-23 13:26:03 +0530

    Time: 2 ms (70.71%) | Memory: 56 MB (8.63%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,19 +1,14 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int n = nums.length;
-        Map<Integer, Integer> map = new HashMap<>();
-        
-        for (int i = 0; i < n; i++) {
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
-        }
-        
-        n = n / 2;
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if (entry.getValue() > n) {
-                return entry.getKey();
+        int candidate = 0, count = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;   // use lowercase 'candidate'
             }
+            count += (num == candidate) ? 1 : -1;
         }
-        
-        return 0;
+
+        return candidate;
     }
 }
