8f1ed256ad38012319af80d24ae912f71b2a8d5e
vtu26585-rgb <vtu26585@veltech.edu.in>
2026-07-28 11:20:35 +0530

    Time: 4 ms (88.14%) | Memory: 46.8 MB (28.18%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String convert(String s, int numRows) {
+        if (numRows == 1) return s;
+        StringBuilder[] rows = new StringBuilder[numRows];
+        for (int i = 0; i < numRows; i++)
+            rows[i] = new StringBuilder();
+        int row = 0, dir = 1;
+        for (char c : s.toCharArray()) {
+            rows[row].append(c);
+            if (row == 0) dir = 1;
+            else if (row == numRows - 1) dir = -1;
+            row += dir;
+        }
+        StringBuilder ans = new StringBuilder();
+        for (StringBuilder r : rows)
+            ans.append(r);
+        return ans.toString();
+    }
+}
