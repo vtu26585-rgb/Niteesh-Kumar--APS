a66a094096a249ff87734c73135bddad483769cc
vtu26585-rgb <vtu26585@veltech.edu.in>
2026-07-28 10:04:41 +0530

    Time: 5 ms (80.78%) | Memory: 45.9 MB (72.64%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,13 +1,11 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if (x < 0 || (x % 10 == 0 && x != 0)) {
-            return false;
+        int original = x;
+        int rev = 0;
+        while(x>0){
+            rev = x%10 + rev*10;
+            x= x/10;
         }
-        int revertedNumber = 0;
-        while (x > revertedNumber) {
-            revertedNumber = revertedNumber * 10 + x % 10;
-            x /= 10;
-        }
-        return x == revertedNumber || x == revertedNumber / 10;
+        return rev==original ? true : false;
     }
 }
