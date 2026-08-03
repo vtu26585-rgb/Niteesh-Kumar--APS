37db2d4dd74e8c52a50f8b6c9d05dae47055c478
vtu26585-rgb <vtu26585@veltech.edu.in>
2026-08-03 09:08:40 +0530

    Time: 0 ms (100.00%) | Memory: 44.5 MB (7.33%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -1,42 +1,18 @@
-
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        // Handle cases where one of the lists is empty
-        if (list1 == null) return list2;
-        if (list2 == null) return list1;
 
-        // Determine the starting node of the merged list
-        ListNode head;
-        if (list1.val <= list2.val) {
-            head = list1;
-            list1 = list1.next;
-        } else {
-            head = list2;
-            list2 = list2.next;
-        }
-
-        // 'curr' will be used to stitch the lists together
-        ListNode curr = head;
-
-        while (list1 != null && list2 != null) {
-            if (list1.val <= list2.val) {
-                curr.next = list1;
-                list1 = list1.next;
-            } else {
-                curr.next = list2;
-                list2 = list2.next;
+        if(list1!=null && list2!=null){
+        if(list1.val<list2.val){
+            list1.next=mergeTwoLists(list1.next,list2);
+            return list1;
             }
-            // Move the current pointer forward
-            curr = curr.next;
+            else{
+                list2.next=mergeTwoLists(list1,list2.next);
+                return list2;
         }
-
-        // If one list is exhausted, attach the remainder of the other
-        if (list1 == null) {
-            curr.next = list2;
-        } else {
-            curr.next = list1;
         }
-
-        return head;
+        if(list1==null)
+            return list2;
+        return list1;
     }
 }
